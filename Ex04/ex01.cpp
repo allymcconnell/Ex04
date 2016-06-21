3aecb7b9c1b3c7c3367298f974ce85b2512d8063
Peter Tucker <ptucker@whitworth.edu>
2016-06-20 17:04:48 -0700

    Added comments (prefixed with PT)

--- Ex04/ex01.cpp
+++ Ex04/ex01.cpp
@@ -11,6 +11,7 @@ using namespace std;
 #include <string>
 
 //Exercise 1
+//PT -- 12/20
 
 int main()
 
@@ -36,10 +37,13 @@ int main()
     
     //calculate the average of the numbers
     average = Sum/arraySize;
+    //PT - this won't compile. All you need is cout << "Average number is: " << average << endl;
     (cout << "Average number is: ")<
     cout << "Above average numbers are: ";
+    //PT and this isn't quite right: for (j=0; j<arraySize; j++)
     for (j= 0, j < arraySize; j++)
     {
+        //PT if (*(numbers + j) > average) ...
         if (*(numbers= &j)> average)
         {
             cout << *(numbers + j) << " ";
